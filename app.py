59e8588507b566e0d49408563aed46a935b8d572
sujit gouda <110072948+SikunKumar@users.noreply.github.com>
2022-10-15 10:12:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,11 +36,11 @@ def main():
     dp.add_error_handler(error)
 
 
-#updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443),
-#                      url_path=TOKEN,
-#                      webhook_url="https://telegrambotpython12.app.heroku.com/" + TOKEN)
+updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443),
+                      url_path=TOKEN,
+                      webhook_url="https://telegrambotpython12.app.heroku.com/" + TOKEN)
 
-#updater.idle()
+updater.idle()
 
 if __name__ == '__main__':
     main()
